917701655c0927c2139db7653cfdf6db13fb1707
Mehedi Hasan <mehedihasanhimel260@gmail.com>
2022-06-01 05:29:05 +0600

    add code in js file

---
+++ class22/script.js
@@ -0,0 +1,4 @@
+let newTask = document.querySelector('#new-task');
+let addTask = document.querySelector('form');
+let comapliteTask = document.querySelector('flexCheckDefault');
+let deletedTask = document.querySelector('#deleted-list')
